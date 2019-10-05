ae2be842a970bcb02967188ff05d23cc28a49aea
junlan <506123087@qq.com>
2019-10-05 22:00:35 +0800

    first commit all

--- src/main/java/com/example/demo/common/Const.java
+++ src/main/java/com/example/demo/common/Const.java
@@ -6,6 +6,6 @@ public class Const {
     public static final String USERNAME = "username";
    public interface Role{
         int ROLE_ADMIN = 1; //管理员
-        int ROLE_CUSTOMER = 0;//普通用户
+        int ROLE_CUSTOMER = 0;//普通用户fsdafsa
     }
 }
